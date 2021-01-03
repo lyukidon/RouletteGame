f3c564cb529bd74dc5a75bb51b87fc4ce987da56
lyukidon <qgx99098@gmail.com>
2021-01-03 13:23:45 +0900

    reset버튼 코드 작성, 기능 구현

--- script/RandomNumber.js
+++ script/RandomNumber.js
@@ -23,28 +23,27 @@ startBtn.addEventListener('click', createRandom );
 function playGame(arr){
     for (let k = 0 ; k < arr.length ; k++ ){
         arr[k].addEventListener('click', function(){
-            // if(arr[k].backgroundColor === 'white'){
-                arr[k].style.backgroundColor = 'skyblue';
-                for (let i=0;i<RandomArr.length;i++){
-                    if (RandomArr[i] === k){
-                        arr[k].style.backgroundColor = 'yellow';
-                    }
+            arr[k].style.backgroundColor = 'skyblue';
+            for (let i=0;i<RandomArr.length;i++){
+                if (RandomArr[i] === k){
+                    arr[k].style.backgroundColor = 'yellow';
                 }
-            // }else{
-            //     alert('이미 누른 곳입니다.');
-            // }
+            }
         })
     }
 }
 
-let classArr;
+let BoxArr;
 startBtn.addEventListener('click', function(){
-    classArr = document.getElementsByClassName('Box');
-    console.log(classArr.length)
-    playGame(classArr);
+    BoxArr = document.getElementsByClassName('Box');
+    playGame(BoxArr);
 })
 
 function resetGame(){
     RandomArr = [];
+    const gameTable = document.getElementById('gameTable');
+    while(gameTable.hasChildNodes()){
+        gameTable.removeChild(gameTable.firstChild); 
+}
 }
 resetBtn.addEventListener( 'click', resetGame );
